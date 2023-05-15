5cc6064d4b48fe292ca814d77198d9670508460e
shivramsingh13 <91517543+shivramsingh13@users.noreply.github.com>
2023-05-15 09:54:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ resource "azurerm_resource_group" "rg-1" {
 
 resource "azurerm_virtual_network" "vnet-1" {
   name = var.vnet_name
-  resource_group_name = azurerm_resource_group.name
-  location = azurerm_resource_group.location
+  resource_group_name = azurerm_resource_group.rg-1.name
+  location = azurerm_resource_group.rg-1.location
   address_space = ["10.0.0.0/16"]
-}
   subnet {
     name = var.subnet-1
     address_prefix = "10.0.1.0/24"
   }
+}
   
